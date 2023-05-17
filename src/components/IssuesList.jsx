dc5855858a1a3025290bca794e8416130976104b
Jennifer Dixon <jdixon@launchdarkly.com>
2023-05-17 09:57:09 -0700

    Saving current status of react query course, getting started, setting up react query.

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,52 @@
 import { Link } from "react-router-dom";
+import { useQuery } from "react-query";
+import { GoIssueOpened, GoIssueClosed } from "react-icons/go";
+
+function IssueItem({
+  title,
+  number,
+  assignee,
+  commentCount,
+  createdBy,
+  createdDate,
+  labels,
+  status,
+}) { 
+  return <li>
+      <div>
+         { status === 'done' ? (
+          <GoIssueOpened style={{color: 'green'}} />
+         ) : (<GoIssueClosed style={{color: 'red'}} />) }
+      </div>
+    </li>; 
+}
 
 export default function IssuesList() {
+  const issuesQuery = useQuery(
+    ["issues"],
+    () => fetch('/api/issues/').then(res => res.json())
+  )
+
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2>Issues List</h2>
+      {issuesQuery.isLoading ? <p>Loading...</p> : 
+        <ul>
+          {issuesQuery.data.map((issue) => 
+            <IssueItem 
+              key={issue.id} 
+              title={issue.title} 
+              number={issue.number} 
+              assignee={issue.assignee} 
+              commentCount={issue.comments.length} 
+              createdBy={issue.createdBy} 
+              createdDate={issue.createdDate} 
+              labels={issue.labels} 
+              status={issue.status} 
+            />
+          )}
+        </ul>
+      }
     </div>
   );
 }
